f542a0feb7a5f38d313eb5186ae2b672e896e21e
Daniel Ricq <dricq@findusapp.de>
2022-08-12 10:44:57 +0200

    feat: Add get route
    Returns a joke as a JSON object

--- api/src/app.js
+++ api/src/app.js
@@ -1,9 +1,18 @@
-import express from "express";
+import express, { response } from "express";
+
+import axios from "axios";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/", async (req, res) => {
+  const jokesApiResponse = await axios(
+    "https://v2.jokeapi.dev/joke/Any?lang=de&blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single"
+  );
+
+  console.log(jokesApiResponse.data.joke);
+  res.json({ joke: jokesApiResponse.data.joke });
 });
 
 app.listen(3000, () => console.log("Example app listening on port 3000!"));
