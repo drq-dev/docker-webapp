80981aac8fe9d12eaa84c162bb3365b7caab76e2
Daniel Ricq <dricq@findusapp.de>
2022-08-12 11:49:48 +0200

    feat: Use api in ui

--- ui/index.js
+++ ui/index.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+const updateJoke = async () => {
+  const response = await fetch("http://localhost:3000");
+  if (!response.ok) return "Witze können nicht geladen werden.";
+
+  const { joke } = await response.json();
+
+  const jokeParagraph = document.querySelector("#joke");
+  jokeParagraph.innerHTML = joke;
+};
+
+const button = document.querySelector("button");
+button.addEventListener("click", updateJoke);
+
+updateJoke();
